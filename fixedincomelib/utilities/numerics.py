c87f26ce7be9a28a048ce59063813cc13bdbe755
SAI_KISHORE <psaikishore2798@gmail.com>
2026-02-08 23:31:35 -0500

    Assignment Complete

--- fixedincomelib/utilities/numerics.py
+++ fixedincomelib/utilities/numerics.py
@@ -87,7 +87,7 @@ class Interpolator1D(ABC):
     @property
     def extrap_method(self) -> str:
         return self.extrap_method_.to_string()
-
+'''
 class Interpolator1DPCP(Interpolator1D):
 
     def __init__(self, axis1: np.ndarray, values: np.ndarray, extrpolation_method: ExtrapMethod) -> None:
@@ -109,6 +109,57 @@ class Interpolator1DPCP(Interpolator1D):
     def gradient_of_integrated_value_wrt_ordinate(self, start_x : float, end_x : float):
         ### TODO
         pass
+
+'''
+
+class Interpolator1DPCP(Interpolator1D):
+    def __init__(self, axis1: np.ndarray, values: np.ndarray, extrpolation_method: ExtrapMethod) -> None:
+        super().__init__(axis1, values, InterpMethod.PIECEWISE_CONSTANT_LEFT_CONTINUOUS, extrpolation_method)
+        assert self.extrap_method_ == ExtrapMethod.FLAT
+
+        if len(axis1) != len(values):
+            print("Length of x input is different from y")
+        else:
+            # Sort arrays based on x
+            idx = np.argsort(axis1)
+            self.axis1_ = np.array(axis1)[idx]
+            self.values_ = np.array(values)[idx]
+            self.length_ = len(self.axis1_)
+
+    def interpolate(self, x_star: float) -> float:
+        bins = self.axis1_
+        bin_idx = np.digitize(x_star, bins, right=False)
+        if bin_idx == 0:
+            return self.values_[0]
+        elif bin_idx >= len(bins):
+            return self.values_[-1]
+        else:
+            return self.values_[bin_idx - 1]
+
+    def gradient_wrt_ordinate(self, x_star: float) -> np.ndarray:
+        bins = self.axis1_
+        bin_idx = np.digitize(x_star, bins, right=False)
+        if bin_idx == 0:
+            loc = 0
+        elif bin_idx >= len(bins):
+            loc = len(self.axis1_) - 1
+        else:
+            loc = bin_idx - 1
+        v = np.zeros(self.length_)
+        v[loc] = 1
+        return v
+
+    def integrate(self, l: float, u: float) -> float:
+        x_right = np.append(self.axis1_[1:], np.inf)
+        lengths = np.maximum(0, np.minimum(u, x_right) - np.maximum(l, self.axis1_))
+        return np.sum(self.values_ * lengths)
+
+    def gradient_of_integrated_value_wrt_ordinate(self, l: float, u: float) -> np.ndarray:
+        x_r = np.append(self.axis1_[1:], np.inf)
+        lengths = np.maximum(0, np.minimum(u, x_r) - np.maximum(l, self.axis1_))
+        return lengths
+
+
 
 class InterpolatorFactory:
 
